13b4fc970a096bef0193eb20780765116b1ef4ce
Ryan Christy <call900913@icloud.com>
2023-08-06 08:31:12 -0400

    Add file: sqrt.go

---
+++ sqrt.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+type ErrNegSqrt float64
+
+// Here we turn ErrNegSqrt into an error by adding this implementation:
+func (e ErrNegSqrt) Error() string {
+	return fmt.Sprintf("That's a negative number you're trying to root: %v", float64(e))
+	// theory: if you put e, it's an error, so the stack will overflow as it continues to execute.
+}
+
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return x, ErrNegSqrt(x)
+	}
+	var z float64 = 1
+	// This method is based on Newton's approximation for finding roots; z = 1 serves as the initial guess.
+	for (z*z-x)*(z*z-x) > 0.01 {
+		z -= ((z*z - x) / (2 * z))
+	}
+	return z, nil
+}
+
+func main() {
+	fmt.Println(Sqrt(2))  // This one gives you a
+	fmt.Println(Sqrt(-2)) // This yields an error, because it's a negative number.
+}
+
