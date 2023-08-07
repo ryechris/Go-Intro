3e959ba9112278a8dea472fd8ce7e305e0a05d35
Ryan Christy <call900913@icloud.com>
2023-08-06 23:22:07 -0400

    Update 02-reader.go so that it can run in the user's machine without resource-consuming imports.

--- 02-reader.go
+++ 02-reader.go
@@ -1,6 +1,10 @@
 package main
 
-import "golang.org/x/tour/reader"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 // https://cs.opensource.google/go/x/tour/+/refs/tags/v0.1.0:reader/validate.go
 
@@ -17,6 +21,38 @@ func (r MyReader) Read(b []byte) (int, error) {
 
 func main() {
 	// this function from Go tests our code's validity:
-	reader.Validate(MyReader{})
+	Validate(MyReader{})
 }
 
+/*******************************************************/
+// The following code was supplied by the Go Authors
+// to validate that our MyReader struct is indeed a Reader,
+// one that emits an infinite stream of 'A'
+
+// Copyright 2014 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+func Validate(r io.Reader) {
+	b := make([]byte, 1024, 2048)
+	i, o := 0, 0
+	for ; i < 1<<20 && o < 1<<20; i++ { // test 1mb
+		n, err := r.Read(b)
+		for i, v := range b[:n] {
+			if v != 'A' {
+				fmt.Fprintf(os.Stderr, "got byte %x at offset %v, want 'A'\n", v, o+i)
+				return
+			}
+		}
+		o += n
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "read error: %v\n", err)
+			return
+		}
+	}
+	if o == 0 {
+		fmt.Fprintf(os.Stderr, "read zero bytes after %d Read calls\n", i)
+		return
+	}
+	fmt.Println("OK!")
+}
